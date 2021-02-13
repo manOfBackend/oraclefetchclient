2f9339f19e3ae8497ce713bd03943c2d47829b00
hyeon <entropy1760@gmail.com>
2021-02-14 01:29:47 +0900

    modify FileType

--- src/main/java/Queue/FileType.java
+++ src/main/java/Queue/FileType.java
@@ -1,6 +1,16 @@
 package Queue;
 
 public enum FileType {
-    CSV,
-    PARQUET
+    CSV("CSV"),
+    PARQUET("PARQUET");
+
+    private final String value;
+
+    FileType(String value) {
+        this.value = value;
+    }
+
+    public String value() {
+        return value;
+    }
 }
