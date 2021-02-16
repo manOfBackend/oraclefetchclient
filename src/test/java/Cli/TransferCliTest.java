069dfe0ccfa881058cc9d86ba33ce613df50591b
manOfBackend <74768098+manOfBackend@users.noreply.github.com>
2021-02-16 19:06:04 +0900

    modify test cases

--- src/test/java/Cli/TransferCliTest.java
+++ src/test/java/Cli/TransferCliTest.java
@@ -8,28 +8,28 @@ class TransferCliTest {
 
     @Test
     void oracle_CSV_ParallelCommand_138MB_Test() {
-        String[] args = "-type CSV -t adid_test -host jdbc:oracle:thin:@localhost:1521:xe -u c##jong -p guswhd12 parallel".split(" ");
+        String[] args = "-o c:\\adid_fetch\\output_adid.csv parallel --reader-type ORACLE -type CSV -t adid_test -host jdbc:oracle:thin:@localhost:1521:xe -u c##jong -p guswhd12".split(" ");
         int exitCode = new CommandLine(new TransferCli()).execute(args);
         assertEquals(exitCode, 0);
     }
 
     @Test
     void oracle_CSV_SingleCommand_138MB_Test() {
-        String[] args = "-type CSV -t adid_test -o c:\\adid_fetch\\output_adid.csv -host jdbc:oracle:thin:@localhost:1521:xe -u c##jong -p guswhd12 single".split(" ");
+        String[] args = "-o c:\\adid_fetch\\output_adid.csv single --reader-type ORACLE -type CSV -t adid_test -host jdbc:oracle:thin:@localhost:1521:xe -u c##jong -p guswhd12".split(" ");
         int exitCode = new CommandLine(new TransferCli()).execute(args);
         assertEquals(exitCode, 0);
     }
 
     @Test
     void oracle_Parquet_SingleCommand_138MB_Test() {
-        String[] args = "-type PARQUET -o c:\\adid_fetch\\output_adid.parquet -t adid_test -host jdbc:oracle:thin:@localhost:1521:xe -u c##jong -p guswhd12 single".split(" ");
+        String[] args = "-o c:\\adid_fetch\\output_adid.csv single --reader-type ORACLE -type PARQUET -o c:\\adid_fetch\\output_adid.parquet -t adid_test -host jdbc:oracle:thin:@localhost:1521:xe -u c##jong -p guswhd12".split(" ");
         int exitCode = new CommandLine(new TransferCli()).execute(args);
         assertEquals(exitCode, 0);
     }
 
     @Test
     void oracle_Parquet_ParallelCommand_138MB_Test() {
-        String[] args = "-type PARQUET -o c:\\adid_fetch\\output_adid.parquet -t adid_test -host jdbc:oracle:thin:@localhost:1521:xe -u c##jong -p guswhd12 parallel".split(" ");
+        String[] args = "-o c:\\adid_fetch\\output_adid.csv parallel --reader-type ORACLE -type PARQUET -o c:\\adid_fetch\\output_adid.parquet -t adid_test -host jdbc:oracle:thin:@localhost:1521:xe -u c##jong -p guswhd12".split(" ");
         int exitCode = new CommandLine(new TransferCli()).execute(args);
         assertEquals(exitCode, 0);
     }
