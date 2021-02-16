ace8ea12196542c5229333835096749374ebfa57
Psolmin <bowwow1994@hanmail.net>
2021-02-16 13:26:00 +0900

    Add HiveReader

--- src/main/java/Downloader/Reader/Impl/HiveReader.java
+++ src/main/java/Downloader/Reader/Impl/HiveReader.java
@@ -2,10 +2,13 @@ package Downloader.Reader.Impl;
 
 import Downloader.Reader.Reader;
 import Queue.BlockingQueue.QueueManager;
+import org.apache.hive.jdbc.HiveDriver;
 
 import java.sql.Connection;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.Properties;
 
 public class HiveReader extends Reader {
 
@@ -16,12 +19,23 @@ public class HiveReader extends Reader {
 
     @Override
     public ResultSet createResultSet(Connection conn, String sql, int fetchSize) throws SQLException {
+        final Statement statement = conn.createStatement();
+        statement.execute("set hive.server2.thrift.resultset.default.fetch.size=" + fetchSize);
+//        statement.execute("set hive.exec.parallel=true");
+//        statement.execute("set hive.vectorized.execution.enabled=true");
+//        statement.execute("set hive.vectorized.execution.reduce.enabled=true");
+        statement.setFetchSize(fetchSize);
 
-        return null;
+        return statement.executeQuery(sql);
     }
 
     @Override
     public Connection createConnection(String hostName) throws SQLException {
-        return null;
+        final HiveDriver hiveDriver = new HiveDriver();
+        final Properties properties = new Properties();
+        properties.setProperty("user", userName);
+        properties.setProperty("password", password);
+
+        return hiveDriver.connect(hostName, properties);
     }
 }
