4cf056671424566d4336c3b3db0793b2c7da5c5c
manOfBackend <74768098+manOfBackend@users.noreply.github.com>
2021-02-17 11:02:59 +0900

    write oracle multi threading logic in ParallelCli

--- src/main/java/Cli/ParallelCli.java
+++ src/main/java/Cli/ParallelCli.java
@@ -1,8 +1,12 @@
 package Cli;
 
+import DbManager.Oracle.OracleManager;
+import DbManager.Oracle.OracleParallelWorker;
 import Downloader.Writer.FileType;
 
-import java.util.concurrent.Callable;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.*;
 
 import static picocli.CommandLine.*;
 
@@ -21,6 +25,9 @@ public class ParallelCli implements Callable<Integer> {
     @Option(names = {"-s", "--fetch-size"}, defaultValue = "10000")
     private int fetchSize;
 
+    @Option(names = {"-sql", "--execute-sql"}, required = true)
+    private String executeSql;
+
     @Option(names = {"-t", "--table-name"}, required = true)
     private String tableName;
 
@@ -34,13 +41,42 @@ public class ParallelCli implements Callable<Integer> {
     private String password;
 
 
-
     @Override
     public Integer call() throws Exception {
         //TODO: MULTI THREADING LOGIC
 
         System.out.println("parallel");
         System.out.println("Threads: " + threadCount);
+
+        OracleManager manager = new OracleManager(hostName, userName, password);
+
+        final int totalRowsCount = manager.getTotalRowsCount(tableName);
+
+        final ExecutorService executorService = Executors.newFixedThreadPool(threadCount);
+
+        int offset = 0;
+        int limit = 0;
+        final int chunkSize = (int) Math.ceil((double) totalRowsCount / threadCount);
+
+        List<CompletableFuture<Void>> futureList = new ArrayList<>();
+
+        for (int i = 0; i < threadCount; i++) {
+            limit += chunkSize;
+            if (limit > totalRowsCount) {
+                limit = totalRowsCount;
+            }
+            futureList.add(CompletableFuture.runAsync(
+                    new OracleParallelWorker(executeSql, offset, limit,
+                            new OracleManager(hostName, userName, password)), executorService)
+            );
+            offset += chunkSize;
+        }
+
+        final CompletableFuture<Void> allFutures = CompletableFuture.allOf(futureList.toArray(CompletableFuture[]::new))
+                .thenRun(() -> { System.out.println("finish"); });
+
+        allFutures.get();
+
         return 0;
     }
 }
