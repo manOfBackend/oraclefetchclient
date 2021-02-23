60f354ecd719f23898876ba11fd9b743308605d0
manOfBackend <74768098+manOfBackend@users.noreply.github.com>
2021-02-23 10:21:26 +0900

    rename ParallelCli -> OracleCli

--- src/main/java/Cli/OracleCli.java
+++ src/main/java/Cli/OracleCli.java
@@ -3,12 +3,14 @@ package Cli;
 import DbManager.Oracle.OracleManager;
 import Downloader.Reader.BlockingQueue.Impl.OracleParallelReader;
 import Downloader.Reader.BlockingQueue.Reader;
+import Downloader.Reader.ReaderType;
 import Downloader.Writer.BlockingQueue.Impl.CSVWriter;
 import Downloader.Writer.BlockingQueue.Impl.ParquetWriter;
 import Downloader.Writer.BlockingQueue.Writer;
 import Downloader.Writer.FileType;
 import Queue.BlockingQueue.Impl.CSVQueueManager;
 import Queue.BlockingQueue.Impl.ParquetQueueManager;
+import Queue.BlockingQueue.QueueManager;
 import avro.Impl.OracleTransformer;
 
 import java.io.File;
@@ -27,8 +29,8 @@ import static picocli.CommandLine.*;
  * 멀티 스레딩 fetch 커맨드 클래스
  * 현재 오라클만 지원함
  */
-@Command(name = "parallel")
-public class ParallelCli implements Callable<Integer> {
+@Command(name = "oracle")
+public class OracleCli implements Callable<Integer> {
 
     @ParentCommand
     private TransferCli transferCli;
@@ -38,6 +40,9 @@ public class ParallelCli implements Callable<Integer> {
 
     @Option(names = {"-type", "--file-type"}, description = "Write File Type (CSV, PARQUET)", required = true)
     private FileType fileType;
+
+    @Option(names = {"-reader", "--reader-type"}, description = "Fetch Reader Type (HIVE, ORACLE)", required = true)
+    private ReaderType readerType;
 
     @Option(names = {"-s", "--fetch-size"}, defaultValue = "10000")
     private int fetchSize;
@@ -57,9 +62,6 @@ public class ParallelCli implements Callable<Integer> {
 
     @Override
     public Integer call() throws Exception {
-        //TODO: MULTI THREADING LOGIC
-        // 싱글과 합쳐도 좋을 듯
-        // 좀 더 안전한지 테스트가 필요함
 
         String executeSql = Files.readString(executeSqlFile.toPath());
         String outputFileName = transferCli.getOutputFileName();
@@ -69,13 +71,12 @@ public class ParallelCli implements Callable<Integer> {
         final ExecutorService readerPool = Executors.newFixedThreadPool(threadCount);
         final ExecutorService writerPool = Executors.newFixedThreadPool(threadCount);
 
-        final int chunkSize = (int) Math.ceil((double) totalRowsCount / threadCount);
+        final int chunkSize = getChunkSize(totalRowsCount);
 
-        List<CompletableFuture<Void>> readerList = new ArrayList<>();
-        List<CompletableFuture<Void>> writerList = new ArrayList<>();
+        List<QueueManager<?>> queueManagerList = createQueueManagers(fileType, threadCount);
 
-        createReadersAndWriters(executeSql, outputFileName, readerPool, writerPool,
-                chunkSize, readerList, writerList);
+        List<CompletableFuture<Void>> readerList = createReaders(executeSql, outputFileName, readerPool, chunkSize, readerType, queueManagerList);
+        List<CompletableFuture<Void>> writerList = createWriters(outputFileName, writerPool, fileType, queueManagerList);
 
         final CompletableFuture<Void> readerAll = CompletableFuture.allOf(readerList.toArray(CompletableFuture[]::new)).thenRunAsync(() -> {
             System.out.println("reader 완료");
@@ -88,6 +89,11 @@ public class ParallelCli implements Callable<Integer> {
         writerAll.cancel(true);
 
         return 0;
+    }
+
+    private int getChunkSize(double totalRowsCount) {
+        final int chunkSize = (int) Math.ceil(totalRowsCount / threadCount);
+        return chunkSize;
     }
 
     private void createReadersAndWriters(String executeSql, String outputFileName, ExecutorService readerPool, ExecutorService writerPool, int chunkSize, List<CompletableFuture<Void>> readerList, List<CompletableFuture<Void>> writerList) throws SQLException {
@@ -112,16 +118,93 @@ public class ParallelCli implements Callable<Integer> {
                     reader = new OracleParallelReader(executeSql, fetchSize, offset, chunkSize, oracleManager, queue);
                     writer = new CSVWriter(outputFileName + i, queue);
                 }
+                case DISRUPTOR_CSV -> {
+                    return;
+                }
                 default -> {
                     return;
                 }
             }
 
-            readerList.add(CompletableFuture.runAsync(reader, readerPool));
+            readerList.add(CompletableFuture.runAsync(reader, readerPool).thenRun(() -> {
+                //reader.close();
+            }));
             writerList.add(CompletableFuture.runAsync(writer, writerPool));
             offset += chunkSize;
         }
     }
+
+
+    private List<QueueManager<?>> createQueueManagers(FileType fileType, int threadCount) {
+
+        List<QueueManager<?>> list = new ArrayList<>();
+
+        for (int i = 0; i < threadCount; i++) {
+            switch (fileType) {
+                case CSV -> {
+                    list.add(new CSVQueueManager());
+                }
+                case PARQUET -> {
+                    list.add(new ParquetQueueManager(new OracleTransformer(), "jong2", "com.jong2"));
+                }
+                case DISRUPTOR_CSV -> {
+                }
+            }
+        }
+        return list;
+    }
+
+    private List<CompletableFuture<Void>> createReaders(String executeSql, String outputFileName, ExecutorService readerPool, int chunkSize, ReaderType readerType, List<QueueManager<?>> queueManagerList) throws SQLException {
+        int offset = 0;
+
+        List<CompletableFuture<Void>> list = new ArrayList<>();
+
+        for (int i = 0; i < queueManagerList.size(); i++) {
+
+            final OracleManager oracleManager = new OracleManager(hostName, userName, password);
+
+            Reader reader = null;
+
+            switch (readerType) {
+                case ORACLE -> {
+                    reader = new OracleParallelReader(executeSql, fetchSize, offset, chunkSize, oracleManager, queueManagerList.get(i));
+                }
+                case HIVE -> {
+                }
+            }
+
+            list.add(CompletableFuture.runAsync(reader, readerPool).thenRun(() -> {
+                //reader.close();
+            }));
+            offset += chunkSize;
+        }
+        return list;
+    }
+
+    private List<CompletableFuture<Void>> createWriters(String outputFileName, ExecutorService writerPool, FileType fileType, List<QueueManager<?>> queueManagerList) {
+        List<CompletableFuture<Void>> list = new ArrayList<>();
+
+        for (int i = 0; i < queueManagerList.size(); i++) {
+            Writer writer = null;
+
+            switch (fileType) {
+                case CSV -> {
+                    writer = new CSVWriter(outputFileName + i, (CSVQueueManager) queueManagerList.get(i));
+                }
+                case PARQUET -> {
+                    writer = new ParquetWriter(outputFileName + i, (ParquetQueueManager) queueManagerList.get(i));
+                }
+                case DISRUPTOR_CSV -> {
+                }
+            }
+
+            list.add(CompletableFuture.runAsync(writer, writerPool).thenRun(() -> {
+                //reader.close();
+            }));
+        }
+        return list;
+    }
+
 
     private int getTotalRowsCount(String executeSql) throws SQLException {
         final OracleManager manager = new OracleManager(hostName, userName, password);
